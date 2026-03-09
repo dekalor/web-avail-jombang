40daa683df8352bc878f36586e45f5398f4f89b6
Ade Eka <ade.eka01@gmail.com>
2026-03-09 13:27:04 +0700

    adjust: db config for production

--- server/db/config.js
+++ server/db/config.js
@@ -26,10 +26,12 @@ module.exports = {
 
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_PROD,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    timezone: '+07:00',
     logging: false,
     define: {
       underscored:   true,   // JS camelCase → DB snake_case automatically
