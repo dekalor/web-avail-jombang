e5b101a8c4fae47d1aa45a1ba55449d58cef4435
Ade Eka <ade.eka01@gmail.com>
2026-06-19 17:39:50 +0700

    feat: add vercel analytics and remove render.yaml

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <Analytics />
   <router-view />
   <error-modal />
 </template>
 
 <script setup>
 import ErrorModal from './components/ErrorModal.vue';
+import { Analytics } from "@vercel/analytics/vue"
 </script>
