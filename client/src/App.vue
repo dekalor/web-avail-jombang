6d11ac7c6bac300e09ed58b735f8ed55c2fec3e8
Ade Eka <ade.eka01@gmail.com>
2026-06-19 17:49:23 +0700

    Implement vercel speed insights

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Analytics />
+  <SpeedInsights />
   <router-view />
   <error-modal />
 </template>
 
 <script setup>
 import ErrorModal from './components/ErrorModal.vue';
-import { Analytics } from "@vercel/analytics/vue"
+import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 </script>
